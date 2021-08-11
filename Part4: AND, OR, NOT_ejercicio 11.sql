6d46a001d3b2eb1a17d0e232980f99ce60fb9f94
liprieto <56852350+liprieto@users.noreply.github.com>
2021-08-11 01:32:19 -0300

    Add files via upload

---
+++ Part4: AND, OR, NOT_ejercicio 11.sql
@@ -0,0 +1,9 @@
+/* OPERADOR AND (solo cuando se cumplen ambas condiciones)*/
+
+select * from country
+where country= 'Argentina' and country_id= 10;
+
+select * from country
+where country= 'Argentina' and country_id = 6;
+
+ 
